b580b790f8008663ac982faa3b5b9f02918c302a
Justin <justin.worrall@gmail.com>
2015-11-18 13:45:49 +0000

    completed echo server example

--- src/echo_server.erl
+++ src/echo_server.erl
@@ -26,13 +26,13 @@ initialise(N) ->
     [echoserver_sup:spawn_child() || _ <- lists:seq(1, N)].
     
 start_link(LSock) ->
-	gen_server:start_link(?MODULE, [LSock], []).
+    gen_server:start_link(?MODULE, [LSock], []).
 
 %% gen_server.
 
 init([LSock]) ->
     io:format("server ~p started~n", [self()]),
-    {ok, #state{lsock=LSock}}.
+    {ok, #state{lsock=LSock}, 0}. %% NB timeout 0
 
 handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
@@ -40,10 +40,28 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info(timeout, #state{lsock=LSock}=State) ->
+    {ok, Socket}=gen_tcp:accept(LSock),
+    inet:setopts(Socket, [{active, once}]),
+    io:format("~p accepted~n", [Socket]),
+    echoserver_sup:spawn_child(),
+    {noreply, State};
+handle_info({tcp, Socket, RawData}, State) ->
+    io:format("Received ~p from ~p~n", [RawData, Socket]),
+    inet:setopts(Socket, [{active, once}]), 
+    gen_tcp:send(Socket, io_lib:fwrite("You sent: ~p~n", [RawData])),
+    {noreply, State};
+handle_info({tcp_closed, Socket}, State) ->
+    io:format("~p closed~n", [Socket]),
+    {stop, normal, State};
+handle_info({tcp_error, Socket}, State) ->
+    io:format("~p error; stopping~n", [Socket]),
+    {stop, normal, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
 terminate(_Reason, _State) ->
+    io:format("~p terminated~n", [self()]),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
--- src/echoserver_sup.erl
+++ src/echoserver_sup.erl
@@ -9,6 +9,8 @@
 
 -define(PORT, 8080).
 
+-define(N_PROCS, 5).
+
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, [?PORT]).
 
@@ -24,4 +26,5 @@ init([Port]) ->
 		worker, 
 		[echo_server]},
     Procs = [WorkerSpec],
+    spawn(fun() -> echo_server:initialise(?N_PROCS) end), %% NB needs to be in a separate process
     {ok, {{simple_one_for_one, 1, 5}, Procs}}. %% NB simple_one_for_one
